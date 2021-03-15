ad85b345baba28ca21f24a754dda6a97f1ff24b8
mariamisa <maryaam1998@hotmail.com>
2021-03-15 23:08:17 +0200

    add cors middleware to api

--- server/app.js
+++ server/app.js
@@ -1,23 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const { join } = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const router = require('./router');
-const { serverError, clientError } = require('./controller/error');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const router = require("./router");
+const { serverError, clientError } = require("./controller/error");
 
 const app = express();
 
-app.set('PORT', process.env.PORT || 8000);
+app.set("PORT", process.env.PORT || 8000);
 const middleware = [
   express.json(),
   express.urlencoded({ extended: false }),
   cookieParser(),
-  logger('dev'),
+  logger("dev"),
+  cors(),
 ];
 app.use(middleware);
 
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
 app.use(clientError);
 app.use(serverError);
